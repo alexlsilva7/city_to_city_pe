76cd1cad376a66fc20e5c9a6e7af793204b197dc
alexlsilva7 <alex.lsilva154@gmail.com>
2022-10-09 12:04:34 -0300

    dados iniciais

---
+++ lib/model/constants.dart
@@ -0,0 +1,28 @@
+const List<String> cidades = [
+  "SAOJOAO",
+  "GARANHUNS",
+  "ANGELIM",
+  "CANHOTINHO",
+  "CAETES",
+  "CALCADO",
+  "JUPI",
+  "BREJAO",
+  "LAJEDO",
+  "PARANATAMA",
+];
+
+const List<List> ligacoesCidade = [
+  ["GARANHUNS", "SAOJOAO", 16],
+  ["GARANHUNS", "CAETES", 19],
+  ["GARANHUNS", "PARANATAMA", 21],
+  ["GARANHUNS", "JUPI", 23],
+  ["GARANHUNS", "BREJAO", 24],
+  ["LAJEDO", "JUPI", 13],
+  ["LAJEDO", "CALCADO", 14],
+  ["LAJEDO", "CANHOTINHO", 32],
+  ["SAOJOAO", "ANGELIM", 11],
+  ["SAOJOAO", "CALCADO", 18],
+  ["CALCADO", "JUPI", 13],
+  ["CALCADO", "CANHOTINHO", 30],
+  ["ANGELIM", "CANHOTINHO", 12],
+];
