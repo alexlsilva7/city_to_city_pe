3feba23e51d3a2a7bcff5e28e4bd6a35c74c2b4a
alexlsilva7 <alex.lsilva154@gmail.com>
2022-10-10 10:32:31 -0300

    mudança visual na home page

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:city_to_city_pe/model/constants.dart';
 import 'package:city_to_city_pe/model/dijkstra_result.dart';
 import 'package:city_to_city_pe/model/grafo_matriz.dart';
 import 'package:flutter/material.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -37,114 +38,198 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color(0xff647DEE),
-            Color(0xff7F53AC),
-          ],
-        ),
-      ),
-      child: Scaffold(
-        // By defaut, Scaffold background is white
-        // Set its value to transparent
-        backgroundColor: Colors.transparent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Scaffold(
+            // By defaut, Scaffold background is white
+            // Set its value to transparent
+            backgroundColor: Colors.transparent,
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                const SizedBox(height: 40),
                 Container(
-                  padding: const EdgeInsets.all(16),
                   width: 300,
+                  padding: const EdgeInsets.all(16),
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(16),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Column(
-                    children: [
-                      // Cidade Partida
-                      DropdownButtonFormField<String>(
-                        items: dropdownItems,
-                        onChanged: (value) {
-                          setState(() {
-                            selectedCityPartida = value!;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          labelText: 'Cidade Partida',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      // Cidade Destino
-
-                      DropdownButtonFormField<String>(
-                        items: dropdownItems,
-                        onChanged: (value) {
-                          setState(() {
-                            selectedCityDestino = value!;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          labelText: 'Cidade Destino',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      // Botão Calcular
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: selectedCityPartida.isNotEmpty &&
-                                  selectedCityDestino.isNotEmpty
-                              ? () {
-                                  setState(() {
-                                    result = matriz.dijkstra(
-                                        selectedCityPartida,
-                                        selectedCityDestino);
-                                  });
-                                }
-                              : null,
-                          child: const Text('Calcular'),
-                        ),
-                      )
-                    ],
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 200,
+                    height: 100,
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ],
-            ),
-            if (result != null)
-              Container(
-                margin: const EdgeInsets.only(top: 16),
-                padding: const EdgeInsets.all(16),
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Column(
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      'Caminho: ${result?.caminhoString()}',
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Distância: ${result?.distancias[result!.indexVerticeFinal]}',
-                      style: const TextStyle(fontSize: 18),
+                    Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      width: 300,
+                      child: Column(
+                        children: [
+                          // Cidade Partida
+                          DropdownButtonFormField<String>(
+                            items: dropdownItems,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedCityPartida = value!;
+                              });
+                            },
+                            decoration: const InputDecoration(
+                              labelText: 'Cidade Partida',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          // Cidade Destino
+                          DropdownButtonFormField<String>(
+                            items: dropdownItems,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedCityDestino = value!;
+                              });
+                            },
+                            decoration: const InputDecoration(
+                              labelText: 'Cidade Destino',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          // Botão Calcular
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: selectedCityPartida.isNotEmpty &&
+                                      selectedCityDestino.isNotEmpty
+                                  ? () {
+                                      setState(() {
+                                        result = matriz.dijkstra(
+                                            selectedCityPartida,
+                                            selectedCityDestino);
+                                      });
+                                    }
+                                  : null,
+                              child: const Text('Calcular'),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ],
                 ),
-              )
-          ],
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 16),
+                    width: 300,
+                    child: ListView(
+                      children: [
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          padding: const EdgeInsets.all(16),
+                          width: 300,
+                          decoration: BoxDecoration(
+                            color: result != null
+                                ? Colors.white
+                                : Colors.transparent,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: AnimatedCrossFade(
+                            duration: const Duration(milliseconds: 200),
+                            crossFadeState: result != null
+                                ? CrossFadeState.showSecond
+                                : CrossFadeState.showFirst,
+                            firstChild: const Text(''),
+                            secondChild: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: result != null
+                                  ? result!.caminhoList().length
+                                  : 0,
+                              itemBuilder: (context, index) {
+                                return TimelineTile(
+                                  alignment: TimelineAlign.manual,
+                                  indicatorStyle: IndicatorStyle(
+                                    width: 20,
+                                    height: 20,
+                                    indicator: Container(
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.deepPurple,
+                                      ),
+                                    ),
+                                  ),
+                                  afterLineStyle: const LineStyle(
+                                    color: Colors.blue,
+                                  ),
+                                  beforeLineStyle: const LineStyle(
+                                    color: Colors.blue,
+                                  ),
+                                  lineXY: 0.1,
+                                  isFirst: index == 0,
+                                  isLast:
+                                      index == result!.caminhoList().length - 1,
+                                  endChild: Container(
+                                    constraints: const BoxConstraints(
+                                        minHeight: 40, maxHeight: 50),
+                                    child: Center(
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            result!.caminhoList()[index],
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 17,
+                                            ),
+                                          ),
+                                          Text(
+                                            '${result!.getDistanciaOf(result!.caminhoList()[index])} km',
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 17,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
